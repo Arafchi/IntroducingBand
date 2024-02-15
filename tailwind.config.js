a21b90361898a1014292e31970dedc41f5633d15
Kim Dongyeon <rlaehd3459@naver.com>
2024-02-16 03:08:07 +0900

    Rename src with docs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/*.html", 
-            "./src/js/*.js"],
+  content: ["./docs/*.html", 
+            "./docs/js/*.js"],
   theme: {
     extend: {
       fontFamily: {
